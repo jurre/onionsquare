801bd68e96f71f09e369bfb97686ed8204055934
Romke Bon <romke@bonsens.nl>
2015-06-20 17:29:54 +0200

    User has products :)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
 
+  has_many :products
+  has_many :ordered_products, through: :orders
+
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
