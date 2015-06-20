381df46d3c508df7c2714e7d43c79c953e791124
Romke Bon <romke@bonsens.nl>
2015-06-20 15:06:49 +0200

    Move card to partial

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,21 @@
 class HomeController < ApplicationController
 
   def index
+
+    @products = [
+      Product.new(
+        title: "German Red Pied cow",
+        description: "Organic grass fed beef."
+      ),
+      Product.new(
+        title: "Blaarkop",
+        description: "Dutch beef by the kilo."
+      ),
+      Product.new(
+        title: "Organic onions",
+        description: "French white onions per square meter"
+      )
+    ]
+
   end
 end
