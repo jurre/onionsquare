ed60004ad42877d2c9ee641f1a040cfb8ab0c904
Romke Bon <romke@bonsens.nl>
2015-06-20 16:51:23 +0200

    Add quantities to product

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150620130544) do
+ActiveRecord::Schema.define(version: 20150620144420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,9 @@ ActiveRecord::Schema.define(version: 20150620130544) do
     t.datetime "image_updated_at"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.decimal  "price"
+    t.integer  "items_available"
+    t.date     "available_until"
   end
 
   create_table "users", force: :cascade do |t|
