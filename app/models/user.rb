761bd5f76284643bda6d71b4d49280f7ebabbe18
jurre <jurrestender+github@gmail.com>
2015-06-20 15:28:40 +0200

    add name to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
+      user.name = auth.info.name
       # user.password = Devise.friendly_token[0,20]
     end
   end
