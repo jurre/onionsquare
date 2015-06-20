4b5aca96f27b7f7de3b50e845f42b23c906c6860
Marcel Horlings <marcel.horlings@gmail.com>
2015-06-20 16:42:55 +0200

    Annotate models

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,3 +10,18 @@ class Product < ActiveRecord::Base
     orders.count / items_available
   end
 end
+
+# == Schema Information
+#
+# Table name: products
+#
+#  id                 :integer          not null, primary key
+#  title              :string
+#  description        :text
+#  image_file_name    :string
+#  image_content_type :string
+#  image_file_size    :integer
+#  image_updated_at   :datetime
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
