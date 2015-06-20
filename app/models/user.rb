49b5fc3042f9aaae9488cb85fa573694a3a67399
Hugo Veeger <hugo@learningspaces.io>
2015-06-20 21:36:49 +0200

    some form fixes + conditional tractor :boar:

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.first_name = auth.info.first_name
-      user.last_name = auth.info.first_name
+      user.last_name = auth.info.last_name
       # user.password = Devise.friendly_token[0,20]
     end
   end
